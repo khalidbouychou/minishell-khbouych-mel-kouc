40dc3a093d6b10be94cc0f596f0106f615fdcb6a
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-05-26 12:37:56 +0100

    first commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 11:35:14 by mel-kouc          #+#    #+#             */
-/*   Updated: 2023/05/26 11:43:04 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2023/05/26 12:37:42 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int	main(int argc, char **argv, char **envp)
 {
 	char	*command;
 	t_env	*head;
+
 	(void)argc;
 	(void)argv;
 	head = env_list(envp);
