f5da37e4658dda987c40a9bc1c1926583ffa80ab
khalidbouychou <khaildbouychou22@gmail.com>
2023-05-28 16:20:24 +0100

    xx

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,14 +6,14 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 14:54:02 by khbouych          #+#    #+#             */
-/*   Updated: 2023/05/28 16:18:21 by khbouych         ###   ########.fr       */
+/*   Updated: 2023/05/28 16:18:48 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incld/minishell.h"
 
-lexer and tokenizer the coomand line
-t_lexer *ft_create_token(char *value)
-{
-    t_lexer	*token;
-}
+// lexer and tokenizer the coomand line
+// t_lexer *ft_create_token(char *value)
+// {
+//     t_lexer	*token;
+// }
