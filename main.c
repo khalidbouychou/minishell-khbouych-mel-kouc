9a6b6f0f3296e833b32876b6f525a499327233d7
khalidbouychou <khaildbouychou22@gmail.com>
2023-05-31 13:59:06 +0100

    c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 15:07:26 by khbouych          #+#    #+#             */
-/*   Updated: 2023/05/29 21:09:05 by khbouych         ###   ########.fr       */
+/*   Updated: 2023/05/31 13:58:36 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,44 +31,6 @@ void print(t_token *lst)
 		printf("tokens == > %s\n",tmp->content);
 		tmp = tmp->next;
 	}
-}
-
-char **ft_split_cmd(char *cmd , char c)
-{
-    int s = 0;
-    int e = 0;
-	char **cg;
-	int i = 0;
-	int x = 0;
-	while(cmd[i] && cmd[i] == 32)
-		i++;
-	while (cmd[i])
-	{
-		s = i;
-		if(cmd[i] && cmd[i] != c)
-			x++;
-		while(cmd[i] && cmd[i] != 32 && cmd[i] != c )
-			i++;
-	}
-	cg = malloc(sizeof(char *) * (x + 1));
-	i = 0;
-	x = 0;
-	while (cmd[i])
-	{
-		while((cmd[i] && cmd[i]) != c || cmd[i] == 32)
-			i++;
-		s = i;
-		while(cmd[i] && cmd[i] != 32 && cmd[i] != c )
-			i++;
-			e = i - 1;
-			if(s < e)
-			{
-				cg[x] =malloc(sizeof(char) * (e - s) + 1);
-				ft_strncpy(cg[x++],&cmd[i],e - s);
-			}
-	}
-	cg[x] = NULL;
-	return cg;
 }
 int	main(int argc, char **argv, char **envp)
 {
