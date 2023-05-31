6083432d6ebaf24217fe67b63ac38d5694c124da
khalidbouychou <khaildbouychou22@gmail.com>
2023-05-31 15:12:33 +0100

    done

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -5,13 +5,50 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/28 16:22:35 by khbouych          #+#    #+#             */
-/*   Updated: 2023/05/30 12:37:35 by mel-kouc         ###   ########.fr       */
+/*   Created: 2023/05/27 18:04:13 by mel-kouc          #+#    #+#             */
+/*   Updated: 2023/05/30 11:12:22 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../incld/minishell.h"
+#include  "minishell.h"
 
-// lexer and tokenizer the coomand line
+// check if pipe is in the last of commande
+int	check_spases(char *commande, int index)
+{
+	int	j;
 
+	j = index + 1;
+	while (commande[j])
+	{
+		if (commande[j] != ' ')
+			return (1);
+		j++;
+	}
+	return (0);
+}
 
+int	error_pipe(char *commande)
+{
+	int	i;
+	int	pipe_index;
+
+	i = 0;
+
+	pipe_index = ft_strchr(commande, '|');
+	if (commande[0] == '|')
+		return (0);
+	if (pipe_index != -1 && !check_spases(commande, pipe_index))
+		return (0);
+	i++;
+	return (1);
+}
+
+char	*lexer(char *commande)
+{
+	if (!error_pipe(commande))
+	{
+		printf("error");
+		exit(1);
+	}
+	return (commande);
+}
