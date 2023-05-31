43e183883e4a899c2f57cc06176a0a848a4866fb
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-05-31 13:16:12 +0100

    delete

--- tokens/token.c
+++ tokens/token.c
@@ -6,7 +6,7 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 15:10:14 by khbouych          #+#    #+#             */
-/*   Updated: 2023/05/31 13:09:26 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2023/05/31 13:15:53 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,89 +77,7 @@
 // }
 
 /*****vvv*/
-t_token	*ft_init_token(char *cmd, int i, int count)
-{
-	t_token	*tok;
-	char	*str;
 
-	tok = NULL;
-	str = NULL;
-	tok = malloc(sizeof(t_token));
-	tok->content = ft_substr(cmd, i, count);
-	tok->type = WORD;
-	tok->path = NULL;
-	tok->operator = 0;
-	tok->next = NULL;
-	return (tok);
-}
-
-void	ft_add_to_list_tokens(t_token **lst_tok, t_token *newtok)
-{
-	t_token	*last;
-
-	if (*lst_tok == NULL)
-		*lst_tok = newtok;
-	else
-	{
-		last = ft_listlast(*lst_tok);
-		last->next = newtok;
-		// newtok->prev = last;
-	}
-}
-
-int	ft_count_alloc(char *cmd, int i, t_token **list)
-{
-	int	count;
-	int	init;
-
-	count = 0;
-	init = i;
-
-	if ((cmd[i] == '>' && cmd[i + 1] == '>') || (cmd[i] == '<'
-			&& cmd[i + 1] == '<'))
-	{
-		count = 2;
-		i = i + count;
-	}
-	else if (cmd[i] == '|' || cmd[i] == ' ' || cmd[i] == '>' || cmd[i] == '<')
-	{
-		count = 1;
-		i = i + count;
-	}
-	else
-	{
-		while (cmd[i] && (cmd[i] != '|' && cmd[i] != ' '
-				&& cmd[i] != '>' && cmd[i] != '<'))
-			i = i + 1;
-		count = i - init;
-	}
-	ft_add_to_list_tokens(list, ft_init_token(cmd, init, count));
-	return (i);
-}
-t_token	*divide(char *cmd)
-{
-	t_token	*lst;
-	int		i;
-
-	lst = NULL;
-	i = 0;
-	while (cmd[i] && cmd[i] == 32)
-		i++;
-	while (cmd[i])
-	{
-		if (cmd[i] == '|')
-			i = ft_count_alloc(cmd, i, &lst);
-		else if (cmd[i] == ' ')
-			i = ft_count_alloc(cmd, i, &lst);
-		else if (cmd[i] == '>')
-			i = ft_count_alloc(cmd, i, &lst);
-		else if (cmd[i] == '<')
-			i = ft_count_alloc(cmd, i, &lst);
-		else
-			i = ft_count_alloc(cmd, i, &lst);
-	}
-	return (lst);
-}
 /*vvvvv**/
 // int	ft_count_alloc(char *cmd, int i, t_token **list)
 // {
