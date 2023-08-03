f638312bba970ae3ead9404b13f7426a8cf16bd1
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-08-03 16:33:24 +0100

    pull  from  mel-kouc

--- builtins/ftenv.c
+++ builtins/ftenv.c
@@ -6,7 +6,7 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 17:45:40 by khbouych          #+#    #+#             */
-/*   Updated: 2023/08/03 14:55:34 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2023/08/03 16:31:59 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 void	ft_env(char **echo, t_env **env)
 {
-	t_env *tmp;
-	// if (!echo[1])
-	// {
+	t_env	*tmp;
+
+	if (!echo[1])
+	{
 		tmp = *env;
-		(void)echo;
-		if(!*env)
-			puts("env khawiiii");
 		while (tmp)
 		{
 			if (tmp->value)
 				printf("%s=%s\n", tmp->key, tmp->value);
 			tmp = tmp->next;
 		}
-		// ft_print_env((*env));
 		g_stu.ex_stu = 0;
-	// }
-	// else
-	// 	ft_exit_output("--> No such file or directory\n", 1, false);
+	}
+	else
+		ft_exit_output("--> No such file or directory\n", 1, false);
 }
+
