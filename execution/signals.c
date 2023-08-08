dc271146750d748c7c8ae6bf68781a37e18f0cb1
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-08-08 09:37:00 +0100

    merge

--- execution/signals.c
+++ execution/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 11:05:52 by khbouych          #+#    #+#             */
-/*   Updated: 2023/08/07 17:05:02 by khbouych         ###   ########.fr       */
+/*   Updated: 2023/08/08 09:36:11 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	sig_handler(int signal)
 {
 	(void)signal;
-	rl_catch_signals = 0;
+	// rl_catch_signals = 0;
 	ft_putstr_fd("\n", 1);
-	rl_replace_line("", 0);
+	// rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 	g_stu.ex_stu = 1;
