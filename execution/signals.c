4599f5c8a6a78bfe6a1e3777a434f87028f9738d
Khalid Bouychou <khbouych@e2r2p15.1337.ma>
2023-08-11 22:16:09 +0100

    done

--- execution/signals.c
+++ execution/signals.c
@@ -6,7 +6,7 @@
 /*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 11:05:52 by khbouych          #+#    #+#             */
-/*   Updated: 2023/08/10 21:01:57 by khbouych         ###   ########.fr       */
+/*   Updated: 2023/08/11 22:15:23 by khbouych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,34 +27,14 @@ void	ft_signal_execution(void)
 
 void	ft_main_sig_handler(int signal)
 {
-	if(signal == SIGINT && g_stu.ex_stu != -1)
-	{
-		rl_catch_signals = 0;
-		ft_putstr_fd("\n", 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-		g_stu.ex_stu = 1;
-	}
+	rl_catch_signals = 0;
+	ft_putstr_fd("\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+	g_stu.ex_stu = 1;
 }
 
-// void	ft_quit_handler(int signal)
-// {
-// 	(void)signal;
-// 	int len;
-// 	len = ft_strlen(rl_line_buffer);
-// 	if (len == 0)
-// 	{
-// 		printf("exit");
-// 		rl_on_new_line();
-// 		rl_redisplay();
-// 	}
-// 	if (len != 0)
-// 	{
-// 		printf("Quit : 3");
-// 		exit(g_stu.ex_stu = 131);
-// 	}
-// }
 void	ft_signals(void)
 {
 	signal(SIGINT, ft_main_sig_handler);
