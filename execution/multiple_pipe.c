40e7ae3480084326e40b85cd2e73be9dee3f11ca
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-08-19 18:31:04 +0100

    push to master last version

---
+++ execution/multiple_pipe.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   multiple_pipe.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/05 21:47:06 by mel-kouc          #+#    #+#             */
+/*   Updated: 2023/08/19 18:19:23 by mel-kouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incld/minishell.h"
+
+void	befor_exec(t_pipe *tmp, t_parse *lst_p)
+{
+	int		i;
+
+	i = 0;
+	close(tmp->prev->fd_p[1]);
+	i = check_fd_exec(lst_p);
+	close(tmp->fd_p[0]);
+	if (i != 1 && i != 3)
+	{
+		dup2(tmp->prev->fd_p[0], STDIN_FILENO);
+		close(tmp->prev->fd_p[0]);
+	}
+	if (i != 2 && i != 3)
+	{
+		dup2(tmp->fd_p[1], STDOUT_FILENO);
+		close(tmp->fd_p[1]);
+	}
+}
+
+int	middle_pipes(t_pipe *tmp, t_parse *lst_p, t_env *env, char **str)
+{
+	lst_p->pid0 = fork();
+	if (lst_p->pid0 == -1)
+		return (-1);
+	else if (lst_p->pid0 == 0)
+	{
+		befor_exec(tmp, lst_p);
+		if (compare_cmd(lst_p))
+		{
+			lst_p->fd_output = 1;
+			cmd_in_built(lst_p, &env);
+			exit(g_v.ex_stu);
+		}
+		else if (execve(lst_p->path, lst_p->arg, str) == -1)
+		{
+			fealed_execve(lst_p);
+			exit(g_v.ex_stu);
+		}
+	}
+	else
+	{
+		close(tmp->prev->fd_p[0]);
+		close(tmp->prev->fd_p[1]);
+	}
+	return (1);
+}
+
+void	loop_cmd(t_pipe	*head, t_parse *lst_p, t_env *env, char **str)
+{
+	int		i;
+	t_pipe	*tmp;
+
+	i = 0;
+	while (lst_p)
+	{
+		if (lst_p->next)
+			tmp = creat_pipe(&head);
+		if (i == 0)
+			first_child(tmp->fd_p, lst_p, env, str);
+		else if (lst_p->next)
+			middle_pipes(tmp, lst_p, env, str);
+		else if (!lst_p->next)
+		{
+			second_child(tmp->fd_p, lst_p, env, str);
+			close(tmp->fd_p[0]);
+			close(tmp->fd_p[1]);
+		}
+		close_fd(lst_p);
+		lst_p = lst_p->next;
+		i = 1;
+	}
+	free_pipe(head);
+}
+
+int	multiple_pipe(t_parse *lst_p, t_env *env, char **str, int size)
+{
+	t_pipe	*head;
+	int		i;
+	// int		status;
+
+	i = 0;
+	head = NULL;
+	loop_cmd(head, lst_p, env, str);
+	i = 0;
+	while (i <= (size - 1))
+	{
+		// pid_t child_pid = waitpid(-1, &g_v.ex_stu, 0);
+		waitpid(-1, &g_v.ex_stu, 0);
+        // printf("GLOBAL = %d\n", g_v.ex_stu);        // if (child_pid > 0)
+		// {
+        //     if (WIFEXITED(status))
+		// 		printf("Child process %d (PID %d) exited with status: %d\n", i, child_pid, WEXITSTATUS(status));
+		// 	else
+		// 		printf("Child process %d (PID %d) terminated abnormally\n", i, child_pid);
+		// }
+		i++;
+	}
+	// while (waitpid(-1, NULL, 0) > 0);
+	// waitpid(lst_p->pid0, &status, 0);
+	return (1);
+}
---
+++ lexer/tokens/util_token.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   util_token.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 18:03:22 by khbouych          #+#    #+#             */
+/*   Updated: 2023/08/17 12:53:24 by khbouych         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incld/minishell.h"
+
+int	ft_qoutes(char *cmd, int *i, int init)
+{
+	int	count;
+
+	count = 0;
+	if (cmd[*i] == '\"' )
+	{
+		*i = *i + 1;
+		while (cmd[*i] != '\"' && cmd[*i])
+			*i = *i + 1;
+		*i = *i + 1;
+		count = *i - init;
+	}
+	else if (cmd[*i] == '\'')
+	{
+		*i = *i + 1;
+		while (cmd[*i] != '\'' && cmd[*i])
+			*i = *i + 1;
+		*i = *i + 1;
+		count = *i - init;
+	}
+	return (count);
+}
+
+int	ft_word(char *cmd, int *i, int init)
+{
+	int	count;
+
+	count = 0;
+	while (cmd[*i] && (cmd[*i] != '|' && cmd[*i] != ' ' \
+		&& cmd[*i] != '>' && cmd[*i] != '<'
+			&& cmd[*i] != '\'' && cmd[*i] != '"'
+			&& cmd[*i] != '	'))
+		*i = *i + 1;
+	count = *i - init;
+	return (count);
+}
+
+void	check_herdoc_quotes(t_token *lst)
+{
+	t_token	*tmp;
+
+	tmp = lst;
+	while (tmp)
+	{
+		if (tmp->type == HERDOC)
+		{
+			tmp = tmp->next;
+			while (tmp->type == SPC || tmp->type == _TAB)
+				tmp = tmp->next;
+			if (tmp->next && tmp->next->type != SPC
+				&& tmp->next->type != _TAB
+				&& (!ft_strcmp(tmp->content, "''")
+					|| !ft_strcmp(tmp->content, "\"\"")))
+				tmp->flag = 1;
+			else if (tmp->content[0] == '\'' || tmp->content[0] == '"')
+				tmp->flag = 1;
+			else if (tmp->next && (!ft_strcmp(tmp->next->content, "''")
+					|| !ft_strcmp(tmp->next->content, "\"\"")))
+				tmp->flag = 1;
+		}
+		tmp = tmp->next;
+	}
+}
+
+void	ft_get_type(t_token *tok)
+{
+	tok->type = WORD;
+	if (ft_strchr(tok->content, '$') != -1
+		&& ((ft_strchr(tok->content, '\'') == -1)
+			|| (ft_strchr(tok->content, '"') < ft_strchr(tok->content, '\'')
+				&& ft_strchr(tok->content, '"') != -1)))
+		tok->type = VAR;
+	else if ('>' == tok->content[0] && '>' == tok->content[1])
+		tok->type = APPND;
+	else if ('<' == tok->content[0] && '<' == tok->content[1])
+		tok->type = HERDOC;
+	else if ('>' == tok->content[0])
+		tok->type = OUTPUT;
+	else if ('<' == tok->content[0])
+		tok->type = INPUT;
+	else if ('|' == tok->content[0])
+		tok->type = PIPE;
+	else if (32 == tok->content[0])
+		tok->type = SPC;
+	else if ('	' == tok->content[0])
+		tok->type = _TAB;
+	if (tok->type != WORD)
+		tok->path = NULL;
+	ft_set_oper(tok);
+}
+
+t_token	*ft_init_token(char *cmd, int i, int count)
+{
+	t_token	*tok;
+
+	tok = malloc(sizeof(t_token));
+	tok->content = NULL;
+	tok->next = NULL;
+	tok->prev = NULL;
+	tok->flag = 0;
+	tok->content = ft_substr(cmd, i, count);
+	ft_get_type(tok);
+	return (tok);
+}
